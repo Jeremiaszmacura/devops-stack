b85af9fb105f0d66feb80b29cd03a1b4875ff5d4
Jeremiasz Macura <jeremiaszmacura@Jeremiaszs-MacBook-Air.local>
2026-02-23 09:42:31 +0100

    linter fixes

--- go-app/internal/server/server.go
+++ go-app/internal/server/server.go
@@ -1,16 +1,18 @@
 package server
 
 import (
-	"go-app/internal/handlers"
-	"go-app/internal/metrics"
+	"fmt"
 	"log"
+
+	"go-app/internal/appmetrics"
+	"go-app/internal/handlers"
 
 	"github.com/gin-gonic/gin"
 )
 
 func StartServer() error {
-	router := gin.Default()                    // Initialize default Gin router (includes Logger and Recovery middleware)
-	router.Use(metrics.PrometheusMiddleware()) // Custom middleware for Prometheus metrics
+	router := gin.Default()                       // Initialize default Gin router (includes Logger and Recovery middleware)
+	router.Use(appmetrics.PrometheusMiddleware()) // Custom middleware for Prometheus metrics
 
 	router.GET("/", handlers.Home)
 	router.GET("/health", handlers.HealthCheck)
@@ -19,5 +21,8 @@ func StartServer() error {
 	router.GET("/redirect", handlers.Redirect)
 
 	log.Println("Starting Go server on :8080")
-	return router.Run(":8080")
+	if err := router.Run(":8080"); err != nil {
+		return fmt.Errorf("failed to start server: %w", err)
+	}
+	return nil
 }
